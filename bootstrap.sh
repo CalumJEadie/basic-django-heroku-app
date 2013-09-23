4cc1ffe35ffefe252a6f4b9b2573888cf5084499
Calum J. Eadie <calum@calumjeadie.com>
2013-09-23 16:17:07 +0100

    Added django requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y python-pip vim
+apt-get install -y python-pip vim libpq-dev
 pip install virtualenv
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
