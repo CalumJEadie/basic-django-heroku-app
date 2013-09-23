80ae97088ff1550811dacf0f36ba2e55d7bce421
Calum J. Eadie <calum@calumjeadie.com>
2013-09-23 16:53:29 +0100

    Configured django

--- basic_django_heroku_app/wsgi.py
+++ basic_django_heroku_app/wsgi.py
@@ -25,8 +25,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basic_django_heroku_app.setting
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
